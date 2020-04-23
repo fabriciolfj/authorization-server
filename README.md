db4e3e2a66ad2704859d42e99442c6fd2879066e
fabricio <fabricio.jacob@outlook.com>
2020-04-22 23:31:50 -0300

    add

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # authorization-server
+
+Client credentials grant -> pega o token com base no nome do client, sem necessidade do usuário final
+é indicado para comunicação entre backends.
+
+Exemplo authorization code: 
+```
+http://localhost:8081/oauth/authorize?response_type=code&client_id=foodanalytics&state=abc&redirect_uri=http://aplicacao-cliente
+```
--- src/main/java/br/com/algaworksauth/algafood/AuthorizationServerConfig.java
+++ src/main/java/br/com/algaworksauth/algafood/AuthorizationServerConfig.java
@@ -3,11 +3,13 @@ package br.com.algaworksauth.algafood;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -19,25 +21,51 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager; // apenas para o password flow
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients
                 .inMemory()
                     .withClient("algafood-web")
                     .secret(passwordEncoder.encode("123"))
-                    .authorizedGrantTypes("password")
+                    .authorizedGrantTypes("password", "refresh_token")
                     .scopes("write", "read")
+                    .accessTokenValiditySeconds(60)
+                    .refreshTokenValiditySeconds(60 * 6)
                 .and()
-                    .withClient("mobile")
+                    .withClient("foodanalytics")
                     .secret(passwordEncoder.encode("123"))
-                    .authorizedGrantTypes("password")
+                    .authorizedGrantTypes("authorization_code", "refresh_token")
                     .scopes("write", "read")
-                .accessTokenValiditySeconds(60 * 60 * 6);
+                    .redirectUris("http://localhost:8082")
+                    .accessTokenValiditySeconds(60)
+                    .refreshTokenValiditySeconds(60 * 6)
+                .and()
+                    .withClient("checktoken")
+                    .secret(passwordEncoder.encode("123"))
+                    .authorizedGrantTypes("password", "refresh_token")
+                    .scopes("write", "read")
+                    .accessTokenValiditySeconds(60)
+                    .refreshTokenValiditySeconds(60 * 6)
+                .and()
+                    .withClient("faturamento")
+                    .secret(passwordEncoder.encode("123"))
+                    .authorizedGrantTypes("client_credentials")
+                    .scopes("write", "read");
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
-                .authenticationManager(authenticationManager);
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
+                //.reuseRefreshTokens(false);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.checkTokenAccess("permitAll()"); //precisa estar autenticado para acessar o check token, permitAll() para náo precisar de autenticação, isAuthenticated() precisa estar autenticado
     }
 }
