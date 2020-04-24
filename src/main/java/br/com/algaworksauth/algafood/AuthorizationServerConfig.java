2ee7f66c455fe2f99308a4bdb7b13aceb1ea9645
fabricio <fabricio.jacob@outlook.com>
2020-04-23 23:52:54 -0300

    add.

--- src/main/java/br/com/algaworksauth/algafood/AuthorizationServerConfig.java
+++ src/main/java/br/com/algaworksauth/algafood/AuthorizationServerConfig.java
@@ -2,6 +2,7 @@ package br.com.algaworksauth.algafood;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,6 +13,8 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 import java.util.Arrays;
 
@@ -27,6 +30,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private RedisConnectionFactory connectionFactory;
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -70,9 +76,14 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
                 .authenticationManager(authenticationManager)
+                .tokenStore(redisTokenStore())
                 .userDetailsService(userDetailsService)
                 .tokenGranter(tokenGranter(endpoints));
                 //.reuseRefreshTokens(false);
+    }
+
+    private TokenStore redisTokenStore() {
+        return new RedisTokenStore(connectionFactory);
     }
 
     @Override
